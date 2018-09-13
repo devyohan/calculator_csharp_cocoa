429687757696370c2ea50a2cf679af32e11c8d22
devyohan <ticonweb@gmail.com>
2018-09-13 13:30:46 +0900

    Init project

---
+++ Calc_test/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Calc_test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
